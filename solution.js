5a77fd583f57fa35724683869ac6411e7096a69b
ryanpradnya <ryanpradnya@gmail.com>
2019-11-20 22:01:34 +0700

    Update scrap data and write to file

--- solution.js
+++ solution.js
@@ -4,35 +4,51 @@ const fs = require('fs');
 
 const url = 'https://www.bankmega.com/promolainnya.php';
 
-request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
+// const allUrl = [
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=1',
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=2',
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=3',
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=4',
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=5',
+//     'https://www.bankmega.com/promolainnya.php?product=0&subcat=6'
+// ];
 
-        const $ = cheerio.load(html);
-        // $('#subcatpromo').find('img').each((i, el) => {
-        //     let subcatId = $(el).attr('id');
-        //     console.log(subcatId);
-        // });
-        let promotion = []
-        $('#promolain').find('a').each((i, el) => {
-            const promoDetailUrl = $(el).attr('href');
-            const title = $(el).find('img').attr('title');
-            const imageUrl = $(el).find('img').attr('src');
+function scrapData(urlToCall, callback) {
+    request(urlToCall, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
 
-            console.log(title);
-            let promo = {
-                title: title,
-                imageUrl: imageUrl,
-                promoDetailUrl: promoDetailUrl
-            }
-            promotion.push(promo);
-        });
-        let data = JSON.stringify(promotion, null, 2);
-        fs.writeFile('solution.json', data, (err) => {
-            if (err) throw err;
-            console.log('Data written to file');
-        })
-        // console.log(subcatpromo);
+            const $ = cheerio.load(html);
+            // $('#subcatpromo').find('img').each((i, el) => {
+            //     let subcatId = $(el).attr('id');
+            //     console.log(subcatId);
+            // });
 
-        console.log('Scraping Done...');
-    }
-});
+            let promotion = []
+            $('#promolain').find('a').each((i, el) => {
+                const promoDetailUrl = $(el).attr('href');
+                const title = $(el).find('img').attr('title');
+                const imageUrl = $(el).find('img').attr('src');
+
+                let promo = {
+                    title: title,
+                    imageUrl: imageUrl,
+                    promoDetailUrl: promoDetailUrl
+                }
+                promotion.push(promo);
+            });
+
+            console.log('Scraping Done...');
+            return callback(promotion);
+
+        }
+    });
+}
+
+scrapData(url, function (response) {
+    let data = JSON.stringify(response, null, 2);
+    fs.writeFile('solution.json', data, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    })
+})
+
