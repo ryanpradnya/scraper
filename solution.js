f0fb23f5f9ef1c5663042c1e71b387865ec6ac72
ryanpradnya <ryanpradnya@gmail.com>
2019-11-21 16:40:16 +0700

    Get sub category url

--- solution.js
+++ solution.js
@@ -5,24 +5,34 @@ const fs = require('fs');
 const url = 'https://www.bankmega.com/promolainnya.php';
 let resultScrap = {};
 
-const allUrl = [
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=1',
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=2',
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=3',
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=4',
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=5',
-    'https://www.bankmega.com/promolainnya.php?product=0&subcat=6'
-];
+function getSubcatPromo(urlToScrap) {
+    return new Promise((resolve) => {
+        request(urlToScrap, (error, response, html) => {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(html);
 
-// request(url, (error, response, html) => {
-//     if (!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-//         $('#subcatpromo').find('img').each((i, el) => {
-//             let subcatId = $(el).attr('id');
-//             console.log(subcatId);
-//         });
-//     }
-// })
+                let subcatPromoId = [];
+                let subcatUrl = [];
+                $('#subcatpromo').find('img').each((i, el) => {
+                    let subcatId = $(el).attr('id');
+                    subcatPromoId.push(subcatId);
+                });
+
+                let text = $('#contentpromolain2').find('script').html()
+                let reg = /promolainnya.php(.*?)"/g;
+                let regresult;
+                while ((regresult = reg.exec(text)) != null) {
+                    if (/product=0&subcat=/.test(regresult[1])) {
+                        subcatUrl.push(url + regresult[1]);
+
+                    }
+                }
+                resolve({ subcatPromoId, subcatUrl });
+            }
+        })
+    })
+}
+
 
 function scrapData(urlToScrap) {
     return new Promise((resolve) => {
@@ -31,8 +41,8 @@ function scrapData(urlToScrap) {
 
                 const $ = cheerio.load(html);
 
-
-                let promotion = []
+                let promotion = [];
+                let allpromo = {};
                 $('#promolain').find('a').each((i, el) => {
                     const promoDetailUrl = $(el).attr('href');
                     const title = $(el).find('img').attr('title');
@@ -45,7 +55,8 @@ function scrapData(urlToScrap) {
                     }
                     promotion.push(promo);
                 });
-                console.log('Scraping Done...');
+                console.log('Scraping ' + urlToScrap + ' Done...');
+                // allpromo[urlToScrap] = promotion
                 resolve(promotion);
 
             }
@@ -54,26 +65,37 @@ function scrapData(urlToScrap) {
     });
 }
 
-
 let fn = async (url) => {
     let resultScrap = await scrapData(url);
     // console.log(url);
     // console.log(resultScrap);
-    return resultScrap
+    return {
+        [url]: resultScrap
+    }
 
 }
 
-let actions = allUrl.map(fn);
+async function setSubcatPromo(url) {
+    let subcatPromo = await getSubcatPromo(url);
+    console.log('subcatPromo: ', subcatPromo);
+    let actions = subcatPromo.subcatUrl.map(fn);
 
-let results = Promise.all(actions);
+    let results = Promise.all(actions);
 
-results.then(resultData => {
+    results.then(resultData => {
 
-    // console.log(resultData)
-    let data = JSON.stringify(resultData, null, 2);
-    fs.writeFile('solution.json', data, (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
-    })
+        // console.log(resultData)
+        let data = JSON.stringify(resultData, null, 2);
+        fs.writeFile('solution.json', data, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        })
+    }
+    );
 }
-);
+
+
+setSubcatPromo(url);
+
+
+
